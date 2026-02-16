f2a5357d81916e161ab7110cd9daea42e65bd9b8
rvxtd <njg44@njit.edu>
2026-02-15 21:57:40 -0500

    Adding Base Unity Files

---
+++ My project/Library/PackageCache/com.unity.xr.androidxr-openxr@06acef508aa4/Editor/ARFoundationFeatureSet.cs
@@ -0,0 +1,54 @@
+using UnityEditor;
+using UnityEngine.XR.OpenXR.Features.Android;
+
+namespace UnityEditor.XR.OpenXR.Features.Android
+{
+    [OpenXRFeatureSet(
+        FeatureIds = new[]
+        {
+            ARSessionFeature.featureId,
+            ARPlaneFeature.featureId,
+            ARCameraFeature.featureId,
+            ARRaycastFeature.featureId,
+            ARAnchorFeature.featureId,
+            DisplayUtilitiesFeature.featureId,
+            AROcclusionFeature.featureId,
+            AndroidXRPerformanceMetrics.featureId,
+            AndroidXRSupportFeature.featureId,
+            ARFaceFeature.featureId,
+            ARBoundingBoxFeature.featureId,
+        },
+        DefaultFeatureIds = new[]
+        {
+            ARSessionFeature.featureId,
+            ARPlaneFeature.featureId,
+            ARCameraFeature.featureId,
+            ARRaycastFeature.featureId,
+            ARAnchorFeature.featureId,
+            DisplayUtilitiesFeature.featureId,
+            AROcclusionFeature.featureId,
+            AndroidXRSupportFeature.featureId,
+            ARFaceFeature.featureId,
+            ARBoundingBoxFeature.featureId,
+        },
+        UiName = "Android XR",
+        FeatureSetId = featureSetId,
+        SupportedBuildTargets = new[] { BuildTargetGroup.Android }
+    )]
+    internal class AndroidFeatureSet
+    {
+        internal const string featureSetId = "com.unity.openxr.featureset.android";
+
+        public static bool IsEnabled
+        {
+            get
+            {
+                var androidXrFeatureSet = OpenXRFeatureSetManager.GetFeatureSetWithId(
+                    EditorUserBuildSettings.selectedBuildTargetGroup,
+                    featureSetId
+                );
+                return androidXrFeatureSet.isEnabled;
+            }
+        }
+    }
+}
